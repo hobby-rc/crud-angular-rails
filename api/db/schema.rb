b36185d68f77b9862f03526361343b3bc93010e1
Robson M. Cavalcante <robson.cavalcante@sefin.ro.gov.br>
2024-07-05 13:30:05 -0400

    feat: add crud tasks

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_07_05_172556) do
+ActiveRecord::Schema[7.0].define(version: 2024_07_05_172944) do
   create_table "tasks", force: :cascade do |t|
     t.string "title"
     t.text "description"
