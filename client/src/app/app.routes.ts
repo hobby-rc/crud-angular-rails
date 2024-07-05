1cd6eeb40f80f12fecf84564b55e5d8b786f0e7e
Robson M. Cavalcante <robson.cavalcante@sefin.ro.gov.br>
2024-07-05 16:27:29 -0400

    temp

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { UsuariosComponent } from './cadastro/usuarios/usuarios.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '/cadastro/usuarios', component: UsuariosComponent }
+];
---
+++ client/src/app/cadastro/usuarios/usuarios.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-usuarios',
+  standalone: true,
+  imports: [],
+  templateUrl: './usuarios.component.html',
+  styleUrl: './usuarios.component.css'
+})
+export class UsuariosComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {
+    
+  }
+}
